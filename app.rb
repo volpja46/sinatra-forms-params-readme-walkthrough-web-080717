8e0447594c5526b29af725100cda62989a33d953
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-08-29 10:01:54 -0400

    done

--- app.rb
+++ app.rb
@@ -7,5 +7,10 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+  #every form needs a corresponding controller route in app.rb
+
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
 
 end
